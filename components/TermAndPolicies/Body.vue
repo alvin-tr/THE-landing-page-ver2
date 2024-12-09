ee854e2637b7968d089e465772546a07da276f53
alvintran888 <106613854+alvintran888@users.noreply.github.com>
2024-12-09 16:45:26 +0700

    term and policie is processing

--- components/TermAndPolicies/Body.vue
+++ components/TermAndPolicies/Body.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="px-[80px] pt-[40px] pb-[64px] w-full bg-[#334b77] flex flex-row justify-around relative
+    <div class="px-[80px] pt-[40px] pb-[64px] w-full bg-[#40dde0] flex flex-row justify-center
         max-size-lg:px-[20px]
     ">
-        <TermAndPoliciesDropdown class="w-[30%] bg-[#3737]"/>
-        <TermAndPoliciesDescription class="w-[80%] bg-[#3737]"/>
+        <TermAndPoliciesDropdown 
+        class="w-[376px] mr-[60px] "
+        @scroll-to-section="handleScrollToSection"
+        />
+        <TermAndPoliciesDescription 
+        class="w-[844px] max-w-[844px]"
+        ref="descriptionSection"
+        />
     </div>
 </template>
 
 <script setup>
-
+const descriptionSection = ref(null)
+const handleScrollToSection = (sectionId) => {
+  const target = descriptionSection.value?.$el.querySelector(sectionId)
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' })
+  }
+}
 </script>
 
 <style lang="scss" scoped>
