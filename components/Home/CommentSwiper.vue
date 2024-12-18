29809058d6999907032f5328b4076bc8b3ebe223
tuanpham3010 <phamtuanhd3010@gmail.com>
2024-12-18 11:19:16 +0700

    done slide swiper

---
+++ components/Home/CommentSwiper.vue
@@ -0,0 +1,140 @@
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+const items = [
+  {
+    title: "Rất tuyệt vời",
+    content:
+      "Thực sự tuyệt vời! Tìm kiếm thông tin dễ dàng và nhanh chóng. Đội ngũ hỗ trợ khách hàng chuyên nghiệp, luôn sẵn sàng giúp đỡ. Cảm thấy an tâm và tin tưởng khi sử dụng dịch vụ vận chuyển của The Human Express.",
+    avatar: "/img/avatar/user-icon1.svg",
+    name: "Le Thi Thuan",
+    company: "Amazon Seller",
+  },
+  {
+    title: "Dễ sử dụng",
+    content:
+      "Đơn giản là tuyệt vời! Dễ dàng tìm thấy thông tin về các dịch vụ và tùy chọn vận chuyển. Giao diện trực quan và thân thiện, giúp mình tiết kiệm thời gian và nỗ lực. Mình sẽ rủ đồng nghiệp dùng thử dịch vụ này.",
+    avatar: "/img/avatar/user-icon2.svg",
+    name: "Dang Minh Hoang",
+    company: "CEO - The Human INC",
+  },
+  {
+    title: "Trải nghiệm tốt",
+    content:
+      "Trải nghiệm rất tốt nhé!! Web cung cấp đầy đủ thông tin về tuyến đường, phí vận chuyển và thời gian giao hàng. Dịch vụ chất lượng cao, tư vấn tận tình, không thể hài lòng hơn với sự chuyên nghiệp này.",
+    avatar: "/img/avatar/user-icon3.svg",
+    name: "Hoang Nguyen",
+    company: "Dropship Seller",
+  },
+  {
+    title: "Thông tin rõ ràng",
+    content:
+      "Vận chuyển quốc tế dễ mà siêu nhanh luôn ạ. Trang web rất dễ sử dụng, có thể theo dõi lộ trình vận chuyển nữa. Mình đánh giá cao thiết kế và đội ngũ phát triển của công ty nhé, hy vọng các bạn có tiếp tục phát triển.",
+    avatar: "/img/avatar/user-icon4.svg",
+    name: "Van Nguyen",
+    company: "CEO - Megamind Digital Limited",
+  },
+  {
+    title: "Tiện dụng",
+    content:
+      "Sản phẩm tiện dụng lắm. Tôi có thể tính toán phí vận chuyển và chọn phương thức vận chuyển phù hợp nhanh chóng. Rất hài lòng với trải nghiệm của mình và sẽ tiếp tục sử dụng dịch vụ nha.",
+    avatar: "/img/avatar/user-icon5.svg",
+    name: "Vu Thu Hien",
+    company: "Amazon Seller",
+  },
+];
+const caroselRef = ref(null);
+
+const indexActive = ref(0);
+const swiperInstance = defineModel();
+const onSwiper = (swiper) => {
+  // console.log(swiper)
+  swiperInstance.value = swiper;
+};
+function handlePrev() {
+  if (swiperInstance.value) swiperInstance.value.slidePrev();
+}
+function handleNext() {
+  if (swiperInstance.value) swiperInstance.value.slideNext();
+}
+const onSlideChange = (swiper) => {
+  // console.log('slide change', )
+  indexActive.value = swiper.realIndex;
+};
+</script>
+
+<template>
+  <BaseLayout class="">
+    <div
+      class="px-[100px] max-size-md:px-[50px] max-size-sm:px-[20px] max-size-pro:px-[10px]"
+    >
+      <div class="mt-[72px] flex items-center max-w-[1024px] mx-auto">
+        <div
+          class="size-[60px] gap-2 min-w-[60px] rounded-full bg-white flex items-center justify-center sm:w-[120px] sm:h-[120px] screen490:w-[80px] screen490:h-80px cursor-pointer"
+          @click="handlePrev"
+        >
+          <UIcon
+            name="mingcute:arrow-left-line"
+            class="text-[#333333cf] text-[25px]"
+          />
+        </div>
+        <Swiper
+          class="mySwiper !overflow-x-hidden"
+          :loop="true"
+          slides-per-view="1"
+          :space-between="24"
+          @swiper="onSwiper"
+          @slideChange="onSlideChange"
+          :modules="[Autoplay]"
+        >
+          <SwiperSlide v-for="item in items">
+            <div class="flex flex-col gap-4 xl:px-10">
+              <p
+                class="font-medium text-[18px] leading-[28px] text-center text-[#0066FF]"
+              >
+                {{ item.title }}
+              </p>
+              <p class="text-[14px] leading-[20px] text-center text-[#333333]">
+                {{ item.content }}
+              </p>
+              <div class="flex flex-col items-center gap-3 mt-4">
+                <UAvatar :src="item.avatar" size="xl" />
+                <p
+                  class="font-medium text-[16px] leading-[24px] text-center text-[#333333]"
+                >
+                  {{ item.name }}
+                </p>
+                <p
+                  class="font-normal text-[12px] leading-[20px] text-center text-[#868689]"
+                >
+                  {{ item.company }}
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div
+          class="size-[60px] min-w-[60px] rounded-full bg-white flex items-center justify-center sm:w-[120px] sm:h-[120px] screen490:w-[80px] screen490:h-80px cursor-pointer"
+          @click="handleNext"
+        >
+          <UIcon
+            name="mingcute:arrow-right-line"
+            class="text-[#333333cf] text-[25px]"
+          />
+        </div>
+      </div>
+      <div class="w-fit mx-auto flex gap-3 mt-9">
+        <div
+          v-for="(item, index) in items"
+          :class="{
+            'bg-slate-300 min-w-[40px] h-[6px] cursor-pointer': true,
+            '!bg-[#0066FF]': indexActive === index,
+          }"
+          @click="() => swiperInstance.slideTo(index)"
+        >
+        </div>
+      </div>
+    </div>
+  </BaseLayout>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,22 @@
 <template>
   <HomeGlobalMarketEstimate class="relative">
-    <Header class="absolute top-0"/>
+    <Header class="absolute top-0" />
   </HomeGlobalMarketEstimate>
-    <div
-      class="w-full flex flex-col items-center bg-[#F7F8FA]">
-        <p
-          class="text-[40px] w-[60%] text-center mt-[100px] text-[#1E1F24] font-medium leading-[60px] tracking-[-0.8px] max-md max-size-pro:text-[30px] max-size-md:text-center"
-        >
-          Dịch vụ vận chuyển quốc tế hàng đầu cho doanh nghiệp Việt
-        </p>
-        <HomeInterest />
-        <HomeTHEService >
-        </HomeTHEService>
-        <HomeCreateOrderToday />
-        <HomeOurPartners />
-        <HomeRateAndComment />
-        <HomeExperienceTheDifference />
-      
-    </div>
+  <div class="w-full flex flex-col items-center bg-[#F7F8FA]">
+    <p
+      class="text-[40px] w-[60%] text-center mt-[100px] text-[#1E1F24] font-medium leading-[60px] tracking-[-0.8px] max-md max-size-pro:text-[30px] max-size-md:text-center"
+    >
+      Dịch vụ vận chuyển quốc tế hàng đầu cho doanh nghiệp Việt
+    </p>
+    <HomeInterest />
+    <HomeTHEService> </HomeTHEService>
+    <HomeCreateOrderToday />
+    <HomeOurPartners />
+    <HomeCommentSwiper />
+    <HomeExperienceTheDifference />
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped></style>
